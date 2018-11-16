97eebbcf6868473f008fb13e35b2c38c64be0a39
pralaymty <pralay.mty@gmail.com>
2018-11-16 15:13:48 +0530

    new project
    NP1

--- ngcurdop/src/app/coin.service.ts
+++ ngcurdop/src/app/coin.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CoinService {
 
-  constructor(private http: HttpClient) { }
+  result: any;
+  constructor(private http: HttpClient) {}
 
   addCoin(name, price) {
     const uri = 'http://localhost:4000/coins/add';
@@ -13,7 +16,54 @@ export class CoinService {
       name: name,
       price: price
     };
-    this.http.post(uri, obj)
-        .subscribe(res => console.log('Done'));
+    this
+      .http
+      .post(uri, obj)
+      .subscribe(res =>
+          console.log('Done'));
+  }
+
+  getCoins() {
+    const uri = 'http://localhost:4000/coins';
+    return this
+            .http
+            .get(uri)
+            .map(res => {
+              return res;
+            });
+  }
+
+  editCoin(id) {
+    const uri = 'http://localhost:4000/coins/edit/' + id;
+    return this
+            .http
+            .get(uri)
+            .map(res => {
+              return res;
+            });
+  }
+
+  updateCoin(name, price, id) {
+    const uri = 'http://localhost:4000/coins/update/' + id;
+
+    const obj = {
+      name: name,
+      price: price
+    };
+    this
+      .http
+      .post(uri, obj)
+      .subscribe(res => console.log('Done'));
+  }
+
+  deleteCoin(id) {
+    const uri = 'http://localhost:4000/coins/delete/' + id;
+
+        return this
+            .http
+            .get(uri)
+            .map(res => {
+              return res;
+            });
   }
 }
